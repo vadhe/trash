3aab7bfedeb7ecbd7136922482466c26afb4c75e
vadhe <aldyvadhe@gmail.com>
2021-04-16 19:27:24 +0800

    chakra ui

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Box, Center, Flex, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg="red.100" minH="3rem">
+      <Box>ini adalah navbar</Box>
+    </Flex>
+  )
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
 import { NextPage } from 'next'
+import Navbar from '~/components/Navbar'
 
-const IndexPage: NextPage = () => (
-  <div>
-    <p>iijii</p>
-  </div>
-)
+const IndexPage: NextPage = () => <Navbar />
 
 export default IndexPage
